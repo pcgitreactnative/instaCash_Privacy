34badc243daba07bb2b8a5442de06a883cb15eaa
USIT <dikshalalit141@gmail.com>
2023-04-26 17:12:17 +0530

    withNpm

---
+++ src/Pages/Privacy.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function Privacy() {
+  return (
+    <div className='privacyBox'>
+    <div className="heading">Privacy Policy</div>
+    <p>The goal of Instacash is to give you the best services while upholding your right to privacy. We want to make sure that all of our clients' information is kept private. We establish a solid foundation of customer trust in this way.
+        By signing up with Instacash, you will get phone, email, and SMS messages from our company. Here are some specifics on the personal data we need; however, we do not save any client data.
+    </p>
+    <ul>
+        <li>Location</li>
+        <li>Date of Birth</li>
+        <li>First Name</li>
+        <li>Mobile Number</li>
+        <li>Last Name</li>
+        <li>Email Address</li>
+    </ul>
+
+    <h2>How will we use your information?</h2>
+    <p>We only utilise our customers' data at Instacash for absolutely necessary things. Read the following to learn more about how we use your personal data:-</p>
+    
+    <ul>
+        <li>Solve queries</li>
+        <li>Manage your data and do our daily work</li>
+        <li>We Send information about new offers, products, and services from our website</li>
+        <li>Enhance Company’s Services</li>
+    </ul>
+    
+    <h2>Read all necessary information.</h2>
+    <p>Before providing your details, you should read the privacy policy in its entirety. If the personal
+        data you give to external sources is lost, we are not responsible. Your personal information should be kept private. It would also be beneficial if you always read the terms and conditions before moving on to the next step.
+    </p>
+
+    <h2>Provide security</h2>
+    <p>Our primary aim is to provide security where all your information is kept confidential. We do not share your data with anyone. So,
+         there are no chances of any cybercrime. We are growing with advanced fintech technology sufficient to maintain cyber security.
+    </p>
+
+    <h2>Email</h2>
+    <p>We use the email to send regular information like new offers, your existing loans statement, repayment reminder,
+         etc. you do not want to receive the email, you can unsubscribe the email information.</p>
+
+    <ol>
+        <li>Company Name-Kickr Technology</li>  
+        <li>Address: A block, Sector 62, Noida, Uttar-Pradesh – 201301.</li>  
+        <li>Phone Number: +91-9012791021</li>
+        <li>Email Address: support@kickrtechnology.com</li>
+    </ol>     
+
+    
+    </div>
+  )
+}
