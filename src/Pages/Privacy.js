8fa9f14aa7b902a53a763e720233be477db94fe9
USIT <dikshalalit141@gmail.com>
2023-05-01 10:17:27 +0530

    new

--- src/Pages/Privacy.js
+++ src/Pages/Privacy.js
@@ -4,8 +4,8 @@ export default function Privacy() {
   return (
     <div className='privacyBox'>
     <div className="heading">Privacy Policy</div>
-    <p>The goal of Instacash is to give you the best services while upholding your right to privacy. We want to make sure that all of our clients' information is kept private. We establish a solid foundation of customer trust in this way.
-        By signing up with Instacash, you will get phone, email, and SMS messages from our company. Here are some specifics on the personal data we need; however, we do not save any client data.
+    <p>The goal of Credit is to give you the best services while upholding your right to privacy. We want to make sure that all of our clients' information is kept private. We establish a solid foundation of customer trust in this way.
+        By signing up with Credit, you will get phone, email, and SMS messages from our company. Here are some specifics on the personal data we need; however, we do not save any client data.   
     </p>
     <ul>
         <li>Location</li>
@@ -17,8 +17,7 @@ export default function Privacy() {
     </ul>
 
     <h2>How will we use your information?</h2>
-    <p>We only utilise our customers' data at Instacash for absolutely necessary things. Read the following to learn more about how we use your personal data:-</p>
-    
+    <p>We only utilise our customers' data at Credit for absolutely necessary things. Read the following to learn more about how we use your personal data:-</p>    
     <ul>
         <li>Solve queries</li>
         <li>Manage your data and do our daily work</li>
@@ -32,14 +31,15 @@ export default function Privacy() {
     </p>
 
     <h2>Provide security</h2>
-    <p>Our primary aim is to provide security where all your information is kept confidential. We do not share your data with anyone. So,
-         there are no chances of any cybercrime. We are growing with advanced fintech technology sufficient to maintain cyber security.
+    <p>
+    Our primary aim is to provide security where all your information is kept confidential.
+     We do not share your data with anyone. So, there are no chances of any cybercrime.
+     We are growing with advanced fintech technology sufficient to maintain cyber security.
     </p>
 
     <h2>Email</h2>
-    <p>We use the email to send regular information like new offers, your existing loans statement, repayment reminder,
-         etc. you do not want to receive the email, you can unsubscribe the email information.</p>
-
+    <p>We use the email to send regular information like new offers, your existing loans statement, repayment reminder, etc. 
+        you do not want to receive the email, you can unsubscribe the email information.</p>
     <ol>
         <li>Company Name-Kickr Technology</li>  
         <li>Address: A block, Sector 62, Noida, Uttar-Pradesh – 201301.</li>  
